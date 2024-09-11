6753e2c6df92228b274f053425edaacead030c92
sgangwa <124158402+sgangwa@users.noreply.github.com>
2024-09-11 14:24:35 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace SME_GitExample_Code.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
